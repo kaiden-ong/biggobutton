9c48ddb443f84625c40cb0939f2688562e507770
Kaiden Ong <kaiden.ong000@gmail.com>
2024-01-25 09:56:51 -0800

    Most features added

---
+++ app/src/main/java/edu/uw/ischool/kong314/biggobutton/MainActivity.kt
@@ -0,0 +1,35 @@
+package edu.uw.ischool.kong314.biggobutton
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        var count = 0
+        button.text = getString(R.string.button_press_message, count)
+        button.setOnClickListener {
+            count++
+            buttonPressed(count, it as Button)
+        }
+    }
+
+    private fun buttonPressed(count: Int, button: Button) {
+        val message = when (count) {
+            1 -> getString(R.string.button_press_message_one)
+            else -> getString(R.string.button_press_message, count)
+        }
+        button.text = message
+        val rnd = Random()
+        val color1 = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        button.setBackgroundColor(color1)
+        val color2 = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        button.setTextColor(color2)
+    }
+}
